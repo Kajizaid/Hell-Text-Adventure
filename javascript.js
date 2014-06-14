a7e0fad2bc2b0ac3773405bd59666c3a43c4d2a1
Yazid <Alabaster456@users.noreply.github.com>
2014-06-14 09:23:18 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -35,7 +35,7 @@ function Area(name, desc, E, W, S, N)
 //List of New Areas
 var entranceOfHell = new Area("Entrance of Hell", "It's cold and dark. I think that's a pile of bones in the corner. There's a glowing light straight ahead", "You can't go East... it's too dark.", "You can't go West... it's too dark.", "You bump into a skeleton... Eek! You turn back around.", stage1);
 
-//Level 1
+//This is the first stage. Consider it level 1.
 var stage1 = new Area("Start of Bridge of Hell", "It's very hot in here... There is fire everywhere. At least there is some light. Looking north, there is a bridge. It seems... broken?", eOfStage1, wOfStage1, entranceOfHell, bridge);
 
 var eOfStage1 = new Area("East of Start of Bridge", "It's just a corner.", "You can't go East.", stage1, stage1, "You can't go North. You'll just bump into a wall!");
@@ -46,7 +46,8 @@ var bridge = new Area("Bridge of Hell", "The bridge is long. Spikes line it. Mon
 //End of Level 1
                      
 
-//End of List
+//End of List of new Areas.
+
 //Starts game function -->
 if (start) 
 {
@@ -65,11 +66,17 @@ function gamePlay(){
     
     //Story part 1
 alert("'~Wake UP, " + player.name + "~.'");
+
 alert("'~WAKE UP!~'");
+
 alert("...You open your eyes, although it's hard to see in the still darkness.");
+
 alert("'~There you are. I am the voice in your head. You have died and been taken to HELL.~'");
+
 alert("You gasp. ...You always imagined Hell to be firey and hot... Yet, it's very chilly.");
-    alert("At least your health is at " + player.health + ".");
+
+alert("At least your health is at " + player.health + ".");
+
 alert("'~Gather yourself, " + player.name + ". I reckon you do not want to be here... forever.~'");
     //END Story part 1
     
